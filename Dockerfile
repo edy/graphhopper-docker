668ea5591eb35cb8d3179440cbc0100043ba3d93
Eduard Baun <eduard@baun.de>
2016-10-16 11:14:19 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM openjdk:8-jdk
+
+WORKDIR /data
+
+RUN mkdir -p /data && \
+	mkdir -p /graphhopper && \
+	cd /graphhopper && \
+	wget https://graphhopper.com/public/releases/graphhopper-web-0.7.0-bin.zip && \
+	unzip *.zip && \
+	rm *.zip
+
+COPY assets/config.properties /graphhopper/
+COPY assets/start.sh /graphhopper/
+
+VOLUME ["/data"]
+EXPOSE 8989
---
+++ assets/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+OSM_FILE=`ls /data/*.pbf`
+
+if [ -f /data/env.sh  ]; then
+	. /data/env.sh
+fi
+
+if [ -z "${JAVA_OPTS}" ]; then
+	JAVA_OPTS="$JAVA_OPTS -Xms64m -Xmx1024m -XX:MaxPermSize=256m -Djava.net.preferIPv4Stack=true"
+	JAVA_OPTS="$JAVA_OPTS -server -Djava.awt.headless=true -Xconcurrentio"
+	echo "Setting default JAVA_OPTS"
+fi
+
+RUN_ARGS=" -jar /graphhopper/*.jar jetty.resourcebase=/graphhopper/webapp config=/graphhopper/config.properties osmreader.osm=$OSM_FILE"
+
+echo "JAVA_OPTS= ${JAVA_OPTS}"
+echo "RUN_ARGS= ${RUN_ARGS}"
+
+java $JAVA_OPTS $RUN_ARGS
